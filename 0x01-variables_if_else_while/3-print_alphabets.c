795e5c7327fcf787e20db9032f8d4621906d8f1e
Samngase <samuelngase@gmail.com>
2023-09-11 19:02:15 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char lowercase = 'a';
+
+    while (lowercase <= 'z')
+    {
+        putchar(lowercase);
+        lowercase++;
+    }
+
+    char uppercase = 'A';
+
+    while (uppercase <= 'Z')
+    {
+        putchar(uppercase);
+        uppercase++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
