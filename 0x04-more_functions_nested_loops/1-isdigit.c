6f32c6edfade28b3c437ab1b0175b7784ff96ef8
Samngase <samuelngase@gmail.com>
2023-09-15 14:35:52 -0700

    Write a function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @c: The number to be checked
+ * Return: 1 for uppercase or 0 for any else
+ */
+
+int _isupper(int x)
+{
+        if (x >= 48 && x <= 57)
+        {
+        return (1);
+        }
+        return (0);
+}
