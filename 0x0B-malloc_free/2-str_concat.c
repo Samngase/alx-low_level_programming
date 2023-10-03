3c7c04f23ab2761f17638fd4ac9fa14bdf33196f
Samngase <samuelngase@gmail.com>
2023-10-03 11:21:10 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,8 +24,8 @@ char *str_concat(char *s1, char *s2)
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
-	return (NULL);
-	i = ci = o;
+		return (NULL);
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
