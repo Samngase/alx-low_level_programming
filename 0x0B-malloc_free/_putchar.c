a97adb36ab0a9314494d110f5ac7197da9d86d60
Samngase <samuelngase@gmail.com>
2023-10-03 10:33:45 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the charcater c to stdout
+ * @cThe charavter to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
